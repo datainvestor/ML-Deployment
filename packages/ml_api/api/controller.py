81ee3a74e194e1de412d505c14e5f154f8a5e987
unknown <vietanhtrinh@protonmail.com>
2019-11-10 17:00:25 +0100

    added

--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request
 
 prediction_app = Blueprint('prediction_app', __name__)
 
-
+#this is one endpoint that when we access we will get 'ok' (routes are defined by @ decorator in flash)
 @prediction_app.route('/health', methods=['GET'])
 def health():
     if request.method == 'GET':
