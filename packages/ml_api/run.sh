04056065bbfb95e27944c8e9499d8bb1df2a281f
unknown <vietanhtrinh@protonmail.com>
2019-11-28 20:24:13 +0100

    a

--- packages/ml_api/run.sh
+++ packages/ml_api/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 export IS_DEBUG=${DEBUG:-false}
 exec gunicorn -b :${PORT:-5000} --access-logfile - --error-logfile - run:application
+
+docker build --build-arg PIP_EXTRA_INDEX_URL=https://xNze1DjSe5zjdByuR4gr@pypi.fury.io/datainvestor/ -t ml_api:latest .
